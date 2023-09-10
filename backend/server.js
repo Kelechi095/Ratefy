886b9d9a483836ceaf26c2fa4bad05878ef6e44f
Awoms Kelechi <awomskelechi@gmail.com>
2023-09-10 21:38:07 +0100

    set up .env & set up cookie parser and cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
+const cookieParser = require('cookie-parser')
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
-app.listen(5000, () => {
-    console.log('serving is listening on port 5000')
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`serving is listening on port ${PORT}`)
 })
