0e5a22b7970075d33de934149c2a501fddc819ba
Awoms Kelechi <awomskelechi@gmail.com>
2023-09-10 21:50:08 +0100

    connected to mongodb database using mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,26 @@
-require('dotenv').config()
-const cookieParser = require('cookie-parser')
-const express = require('express')
-const cors = require('cors')
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/connectDB");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(PORT, () => {
-    console.log(`serving is listening on port ${PORT}`)
-})
+startServer();
